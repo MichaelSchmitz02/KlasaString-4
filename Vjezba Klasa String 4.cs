7e80e3e0d83783cfab5bf775180e22db8f5deada
MichaelSchmitz02 <152979941+MichaelSchmitz02@users.noreply.github.com>
2024-01-16 12:33:36 +0100

    Add files via upload

---
+++ Vjezba Klasa String 4.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Unesite rečenicu: ");
+        string unos = Console.ReadLine();
+
+
+        string[] rijeci = unos.Split(' ');
+
+
+    if (rijeci.Length > 0)
+{
+    Console.WriteLine("Prva riječ: " + rijeci[0]);
+}
+else
+{
+    Console.WriteLine("Rečenica je prazna.");
+}
+if (rijeci.Length > 0)
+{
+    Console.WriteLine("Zadnja riječ: " + rijeci[rijeci.Length - 1]);
+}
+else
+{
+    Console.WriteLine("Rečenica je prazna.");
+}
+    }
+}
+
+
+       
+      
+
+
